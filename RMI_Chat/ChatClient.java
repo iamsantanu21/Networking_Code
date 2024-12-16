59561494d595336b8bcd47bd1a32a01393b15276
Santanu Mondal <54280464+iamsantanu21@users.noreply.github.com>
2024-12-16 12:40:44 +0530

    main

--- RMI_Chat/ChatClient.java
+++
@@ -1,6 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface ChatClient extends Remote {
-    void receiveMessage(String message) throws RemoteException;
-}
--- RMI_user/AuthService.java
+++
@@ -1,8 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface AuthService extends Remote {
-    boolean verifyEmail(String email) throws RemoteException;
-    boolean verifyPassword(String email, String password) throws RemoteException;
-    String verifyCredentials(String email, String password) throws RemoteException; // Add RemoteException here
-}
